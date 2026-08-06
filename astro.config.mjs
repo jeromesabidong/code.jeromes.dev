e9ddaa1b71003b815ad7b494123a4022b78209c9
jeromesabidong <jeromesabidong@gmail.com>
2026-08-06 18:17:53 +0900

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  output: 'static',
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      langs: [],
+      wrap: true,
+    },
+  },
+});
