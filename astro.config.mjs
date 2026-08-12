fef4042b5a541add71d6c964e6bdbb46bbe57737
jeromesabidong <jeromesabidong@gmail.com>
2026-08-12 22:47:56 +0900

    - added sitemap, robots, and seo tools - added lighthouse and lychee dependencies for testing prior to s3 sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: process.env.SITE_URL || 'https://localhost:3000',
   output: 'static',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = true;
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL('sitemap-index.xml', site);
+
+  return new Response(
+    `User-agent: *\nAllow: /\n\nSitemap: ${sitemapURL}\n`,
+    { headers: { 'Content-Type': 'text/plain' } },
+  );
+};
